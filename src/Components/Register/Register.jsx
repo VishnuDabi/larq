8b072551d26a1e5077afc92807add89c30577a26
dd-4191 <xyz14203@gmail.com>
2024-01-12 18:23:14 +0530

    update

---
+++ src/Components/Register/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <section className="register-main-container blue">
+      <h2>New Account</h2>
+
+      <div className="register-wrapper blue"></div>
+    </section>
+  );
+};
+
+export default Register;
