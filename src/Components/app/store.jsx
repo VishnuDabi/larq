0f9cd255417513b57c38d2ae762c7176dabf22df
dd-4191 <xyz14203@gmail.com>
2024-02-19 12:05:32 +0530

    redux-presist add

--- src/Components/app/store.jsx
+++ src/Components/app/store.jsx
@@ -1,7 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import cartReducer from "../../Redux/features/counter/cartSlice";
+// import authReducer from "../../Redux/features/counter/authSlice";
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(
+  persistConfig,
+  cartReducer
+  // authReducer
+);
+
 export const store = configureStore({
   reducer: {
-    cart: cartReducer,
+    cart: persistedReducer,
   },
 });
+
+export const persistor = persistStore(store);
